c8f95b1b0c5c6b7915e2038f02295efe8e313b9f
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-11-25 15:47:05 -0600

    Basic website setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Welcome to Hemesh's Portfolio</h1>
+      <h2>Stay tuned as I continue to build this website out :)</h2>
     </div>
   );
 }
