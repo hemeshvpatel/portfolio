68c15bf220417409b9b7d9803d89e794dfdbfbac
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-11-25 17:28:01 -0600

    basic menu added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import HomePage from "./Components/homepage";
+import MenuBar from "./Components/menubar";
 
 function App() {
   return (
     <div>
-      <h1>Welcome to Hemesh's Portfolio</h1>
-      <h2>Stay tuned as I continue to build this website out :)</h2>
+      <MenuBar />
+      <HomePage />
     </div>
   );
 }
---
+++ src/Components/menubar.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { Menu } from "semantic-ui-react";
+
+class MenuBar extends Component {
+  state = {};
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  render() {
+    const { activeItem } = this.state;
+
+    return (
+      <Menu fluid stackable>
+        <Menu.Item>
+          <img src="avatar.png" alt="" />
+        </Menu.Item>
+
+        <Menu.Item
+          name="Resume"
+          active={activeItem === "Resume"}
+          onClick={this.handleItemClick}
+        >
+          Resume
+        </Menu.Item>
+
+        <Menu.Item
+          name="Projects"
+          active={activeItem === "Projects"}
+          onClick={this.handleItemClick}
+        >
+          Projects
+        </Menu.Item>
+
+        <Menu.Item
+          name="LinkedIn"
+          active={activeItem === "LinkedIn"}
+          onClick={this.handleItemClick}
+        >
+          LinkedIn
+        </Menu.Item>
+
+        <Menu.Item
+          name="GitHub"
+          active={activeItem === "GitHub"}
+          onClick={this.handleItemClick}
+        >
+          GitHub
+        </Menu.Item>
+      </Menu>
+    );
+  }
+}
+
+export default MenuBar;
