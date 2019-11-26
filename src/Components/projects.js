f7b85f3f37ef6a5601d750442f12af597c797b0b
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-11-26 01:18:36 -0600

    Updated project cards, added links, organized, still needs tweaking

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 import {
   Container,
   Header,
@@ -15,72 +16,133 @@ function Projects() {
       <Header as="h2">My Projects</Header>
       <br />
       <Card.Group centered stackable>
-        <Card>
+        <Card color="green">
           <Card.Content>
-            <Image
-              //   floated="right"
-              size="mini"
-              src="images/shoppingcart.png"
-            />
+            <Icon name="gem" size="large" />
+            <Icon name="html5" size="large" />
+            <Icon name="js" size="large" />
+            <Icon name="react" size="large" />
+          </Card.Content>
+          <Card.Content>
             <Card.Header>Grocery Delivery</Card.Header>
             <Card.Meta>E-Commerce Website</Card.Meta>
             <Card.Description>
-              Capstone project at Flatiron School
+              Capstone project at Flatiron School. Built in under a week. Uses
+              Redux for shopping cart functionality.
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
-            <div className="ui two buttons">
-              <Button basic color="green">
-                View Live Version
+            <Button.Group widths="2">
+              <Button
+                basic
+                color="black"
+                href="https://grocery-delivery-frontend.herokuapp.com/"
+                target="_blank"
+              >
+                <Icon color="green" name="play" />
+                Load Site
               </Button>
-              <Button basic color="red">
-                View on GitHub
+              <Button basic color="black">
+                <Icon color="red" name="github" />
+                View GitHub
               </Button>
-            </div>
+            </Button.Group>
           </Card.Content>
         </Card>
-        <Card>
+
+        <Card color="green">
           <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="/images/avatar/large/molly.png"
-            />
+            <Icon name="gem" size="large" />
+            <Icon name="html5" size="large" />
+            <Icon name="js" size="large" />
+          </Card.Content>
+          <Card.Content>
             <Card.Header>AppleXchange</Card.Header>
             <Card.Meta>Trading Website</Card.Meta>
             <Card.Description>
-              A website where you can buy/sell/trade Apple Products
+              A website where you can buy, sell, and trade Apple Products. Built
+              using Vanilla JavaScript and HTML.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content>
+            <Button.Group widths="2">
+              <Button
+                basic
+                color="black"
+                href="https://applexchange.herokuapp.com/"
+                target="_blank"
+              >
+                <Icon color="green" name="play" />
+                Load Site
+              </Button>
+              <Button basic color="black">
+                <Icon color="red" name="github" />
+                View GitHub
+              </Button>
+            </Button.Group>
+          </Card.Content>
+        </Card>
+        <Card color="green">
+          <Card.Content>
+            <Icon name="gem" size="large" />
+            <Icon name="html5" size="large" />
+            <Icon name="js" size="large" />
+            <Icon name="react" size="large" />
+          </Card.Content>
+          <Card.Content>
+            <Card.Header>Grocery Delivery</Card.Header>
+            <Card.Meta>E-Commerce Website</Card.Meta>
+            <Card.Description>
+              Capstone project at Flatiron School. Built in under a week. Uses
+              Redux for shopping cart functionality.
             </Card.Description>
           </Card.Content>
           <Card.Content extra>
             <div className="ui two buttons">
-              <Button basic color="green">
-                Live
+              <Button
+                basic
+                color="black"
+                href="https://grocery-delivery-frontend.herokuapp.com/"
+                target="_blank"
+              >
+                <Icon color="green" name="play" />
+                Load Site
               </Button>
-              <Button basic color="red">
+              <Button basic color="black">
+                <Icon color="red" name="github" />
                 GitHub
               </Button>
             </div>
           </Card.Content>
         </Card>
-        <Card>
+
+        <Card color="green">
           <Card.Content>
-            <Image
-              floated="right"
-              size="mini"
-              src="/images/avatar/large/jenny.jpg"
-            />
-            <Card.Header>Active Project</Card.Header>
-            <Card.Meta>New Project</Card.Meta>
-            <Card.Description>Details to be announced soon!</Card.Description>
+            <Icon name="gem" size="large" />
+            <Icon name="html5" size="large" />
+            <Icon name="js" size="large" />
           </Card.Content>
-          <Card.Content extra>
+          <Card.Content>
+            <Card.Header>AppleXchange</Card.Header>
+            <Card.Meta>Trading Website</Card.Meta>
+            <Card.Description>
+              A website where you can buy, sell, and trade Apple Products. Built
+              using Vanilla JavaScript and HTML.
+            </Card.Description>
+          </Card.Content>
+          <Card.Content>
             <div className="ui two buttons">
-              <Button basic color="green">
-                Live
+              <Button
+                basic
+                color="black"
+                href="https://applexchange.herokuapp.com/"
+                target="_blank"
+              >
+                <Icon color="green" name="play" />
+                Load Site
               </Button>
-              <Button basic color="red">
-                <Icon color="black" name="github" />
+              <Button basic color="black">
+                <Icon color="red" name="github" />
                 GitHub
               </Button>
             </div>
--- src/Components/resume.js
+++ src/Components/resume.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Header } from "semantic-ui-react";
 
-function Resume() {
-  return (
-    <Container fluid textAlign="center">
-      <br />
-      <Header as="h2">Welcome to Hemesh's Resume</Header>
-      <p>adding a pdf viewer and downloader here for resume</p>
-    </Container>
-  );
+//react-pdf - Standard
+import { pdfjs, Document, Page } from "react-pdf/dist/entry.webpack";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+class Resume extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1
+  };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    return (
+      <Container fluid textAlign="center">
+        <br />
+        <Header as="h2">My Resume</Header>
+        <p>adding a pdf viewer and downloader here for resume</p>
+        <br />
+        <div>
+          <Document
+            file="../../images/resume"
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            <Page pageNumber={pageNumber} />
+          </Document>
+          <p>
+            Page {pageNumber} of {numPages}
+          </p>
+        </div>
+      </Container>
+    );
+  }
 }
 
 export default Resume;
